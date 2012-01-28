8163a3c198227d161ef4ad550e43711470f8df54
Flavio Lerda <flerda@gmail.com>
2012-01-28 22:09:04 +0000

    Extra a helper function for tests.
    This allows the tests to be more readable, by directly referring to the
    expected value together with the parameters used for the test.
    
    It uses two characteristics of C#: the fact that arrays can be defined
    inline (almost as literals) and the fact that Assert.AreEqual is able to
    correctly compare List and arrays based on their contents.
    
    Having a single place where this is done because that any improvements
    can be done once and it will be applied to all of its usages: for
    instance I lost one property of one of your tests, which allowed the
    function to return the results in either order; it can be added back by
    only changing the helper function and all tests (existing or newly
    added) will get the same feature.

--- SumSet/Tests.cs
+++ SumSet/Tests.cs
@@ -10,60 +10,39 @@ namespace ConsoleApplication1
     [TestFixture]
     public class ProgramTest
     {
+        private void CheckFindSetsThatSumToTotal(int[][] expected, int sizeOfSet, int total, int largestValue) {
+            var actual = Program.FindSetsThatSumToTotal(sizeOfSet, total, largestValue);
+            Assert.AreEqual(expected, actual);
+        }
+
         [Test]
         public void LookForSetOfSizeOneOfAOneDigitNumber_ExpectOneListOfThatNumberReturned()
         {
-            var listOfSetsReturned = Program.FindSetsThatSumToTotal(1, 2, 9);
-
-            Assert.AreEqual(listOfSetsReturned.Count, 1);
-            Assert.AreEqual(listOfSetsReturned[0], new List<int> { 2 });
+            CheckFindSetsThatSumToTotal(new[]{ new[]{2} }, 1, 2, 9);
         }
 
         [Test]
         public void LookForSetOfSizeOneOfATwoDigitNumber_ExpectNullReturned()
         {
-            var listOfSetsReturned = Program.FindSetsThatSumToTotal(1, 12, 9);
-
-            Assert.AreEqual(listOfSetsReturned, null);
+            CheckFindSetsThatSumToTotal(null, 1, 12, 9);
         }
 
         [Test]
         public void LookForSetOfSizeGreaterThanOneWhichReturnsOneResult_ExpectOneListReturned()
         {
-            var listOfSetsReturned = Program.FindSetsThatSumToTotal(2, 17, 9);
-
-            Assert.AreEqual(listOfSetsReturned.Count, 1);
-            Assert.AreEqual(listOfSetsReturned[0].Count, 2);
-            Assert.AreEqual(listOfSetsReturned[0].Contains(9), true);
-            Assert.AreEqual(listOfSetsReturned[0].Contains(8), true);
+            CheckFindSetsThatSumToTotal(new[]{ new[]{9, 8} }, 2, 17, 9);
         }
 
         [Test]
         public void LookForSetOfSizeGreaterThanOneWhichReturnsMultipleResults_ExpectCorrectListsReturned()
         {
-            var listOfSetsReturned = Program.FindSetsThatSumToTotal(2, 16, 9);
-
-            Assert.AreEqual(listOfSetsReturned.Count, 2);
-            
-            var firstList = listOfSetsReturned[0];
-            var secondList = listOfSetsReturned[1];
-
-            Assert.AreEqual(firstList.Count, 2);
-            Assert.AreEqual(secondList.Count, 2);
-                        
-            Assert.AreEqual(firstList[0] + firstList[1], 16);
-            Assert.AreEqual(secondList[0] + secondList[1], 16);
-			
-            Assert.IsFalse(firstList[0] == secondList[0] && firstList[1] == secondList[1]);
-            Assert.IsFalse(firstList[1] == secondList[0] && firstList[0] == secondList[1]);
+            CheckFindSetsThatSumToTotal(new[]{ new[]{8, 8}, new[]{9, 7} }, 2, 16, 9);
         }
 
         [Test]
         public void LookForSetOfSizeGreaterThanOneWhichReturnsNoResults_ExpectNullReturned()
         {
-            var listOfSetsReturned = Program.FindSetsThatSumToTotal(2, 21, 9);
-
-            Assert.AreEqual(listOfSetsReturned, null);
+            CheckFindSetsThatSumToTotal(null, 2, 21, 9);
         }
 
     }
